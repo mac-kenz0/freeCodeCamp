f27d9cd81e52f546e3963ba472f723fefaa3050b
mac-kenz0 <jordanm8118@gmail.com>
2017-04-07 14:05:32 -0400

    got display function working correctly, need to tweak count

--- final-code-pen-projects/simon/js/js.js
+++ final-code-pen-projects/simon/js/js.js
@@ -1,59 +1,68 @@
-var i, strictMode, displayArray = [],
+//var i, strictMode,
+//   a = [];
+//var count = 0;
+const maxRounds = 5;
+
+function generateArray() {
     a = [];
-const maxRounds = 3; // 
+    for (i = 0; i < maxRounds; i++) {
+        a[i] = Math.floor(Math.random() * 4);
+    }
 
-
-for (i = 0; i < maxRounds; i++) {
-    a[i] = Math.floor(Math.random() * 4);
+    return a;
 }
+
 $('#strict').click(function() {
-	 strictMode = true;
+    strictMode = true;
 });
+
 
 $('#start').click(function() {
-    var x = 0;
-    var count = 1;
-
-    //function displayArray(count);
-    function go() {
-
-
-        if (x < maxRounds) {
-            toggleButton(a, x);
-            setTimeout(go, 2000);
-            x++;
-        }
-    }
-    //go();
-    go();
-    // after that function is complete...
-    setTimeout(function() {
-        listenUserInput();
-    }, 2000 * maxRounds);
+    var a = generateArray();
+    count = 1;
+    displayArray(a, count);
 });
-function displayArray(count){
 
+function displayArray(a, count) {
+    var time = 1000;
+    var i;
+    console.log(a);
+
+    var runArray = a.slice(0, count);
+
+    $.each(runArray, function(i, value) {
+        setTimeout(function() {
+            console.log(value);
+            toggleButton(value);
+            
+            console.log("\n");
+        }, time)
+        time += 1500;
+    });
+               
+    setTimeout(function() {
+        listenUserInput(runArray);
+    }, time);
 }
 
-
-
-function toggleButton(a, i) {
+function toggleButton(i) {
+    console.log("toggle" + i);
     var p;
     var audio;
 
-    if (a[i] == 0) {
+    if (i == 0) {
         p = $(".top-left-circle");
         audio = new Audio('../music/simonSound1.mp3');
     }
-    if (a[i] == 1) {
+    if (i == 1) {
         p = $(".top-right-circle");
         audio = new Audio('../music/simonSound2.mp3');
     }
-    if (a[i] == 2) {
+    if (i == 2) {
         p = $(".bottom-left-circle");
         audio = new Audio('../music/simonSound3.mp3');
     }
-    if (a[i] == 3) {
+    if (i == 3) {
         p = $(".bottom-right-circle");
         audio = new Audio('../music/simonSound4.mp3');
     }
@@ -68,83 +77,132 @@ function toggleButton(a, i) {
     });
 }
 
-
+function listenUserInput (runArray){
+    console.log(runArray);
+    var r = [];
+     $(".top-left-circle").click(function() {
+        toggleButton(0);
+        r.push(0);
+        check(runArray, r);
+     });
+     $(".top-right-circle").click(function() {
+        toggleButton(1);
+        r.push(1);
+        check(runArray, r);
+     });
+     $(".bottom-left-circle").click(function() {
+        toggleButton(2);
+        r.push(2);
+        check(runArray, r);
+     });
+     $(".bottom-right-circle").click(function() {
+        toggleButton(3);
+        r.push(3);
+        check(runArray, r);
+     });
+}
 
 // after the display is over, then listen for user input
-function listenUserInput() {
-    var p, result = [];
+// listenUserInput(runArray);
+// function listenUserInput(runArray) {
+//     console.log("listening...");
+//     var p, result = [];
+//     //console.log("count:" + count);
+//     var lengthOfArray = runArray.length;
+//     var runArrayIndex;
+//     console.log("runArray");
+//     console.log(runArray);
+//     console.log("\n");
+//     var isInputCorrect;
+    
+//     $(".top-left-circle").click(function() {
+//         p = $(".top-left-circle");
+//         p.fadeOut('slow', function() {
+//             p.css("opacity", ".75");
+//         });
 
-    $(".top-left-circle").click(function() {
-    	p = $(".top-left-circle");
-    	p.fadeOut('slow', function() {
-        p.css("opacity", ".75");
-    	});
+//         p.fadeIn('slow', function() {
+//             p.css("opacity", "1");
+//         });
+        
+//         result.push(0);
+//        // userCount++;
+//         audio = new Audio('../music/simonSound1.mp3');
+//         audio.play();
 
-    p.fadeIn('slow', function() {
-        p.css("opacity", "1");
-    });
-        result.push(0);
-        audio = new Audio('../music/simonSound1.mp3');
-        audio.play();
-        checkUserInput(a, result);
-    });
+//         // compare arrays  
+//         isInputCorrect = checkUserInput(runArray, result);
+//     });
 
-    $(".top-right-circle").click(function() {
-    	p = $(".top-right-circle");
-    	 p.fadeOut('slow', function() {
-        p.css("opacity", ".75");
-    });
+//     $(".top-right-circle").click(function() {
+//         p = $(".top-right-circle");
+//         p.fadeOut('slow', function() {
+//             p.css("opacity", ".75");
+//         });
 
-    p.fadeIn('slow', function() {
-        p.css("opacity", "1");
-    });
-        result.push(1);
-        audio = new Audio('../music/simonSound2.mp3');
-        audio.play();
-        checkUserInput(a, result);
-    });
-    $(".bottom-left-circle").click(function() {
-    	p = $(".bottom-left-circle");
-    	    p.fadeOut('slow', function() {
-        p.css("opacity", ".75");
-    });
+//         p.fadeIn('slow', function() {
+//             p.css("opacity", "1");
+//         });
+//         result.push(1);
+//         audio = new Audio('../music/simonSound2.mp3');
+//         audio.play();
+//          isInputCorrect = checkUserInput(runArray, result);
+//     });
+//     $(".bottom-left-circle").click(function() {
+//         p = $(".bottom-left-circle");
+//         p.fadeOut('slow', function() {
+//             p.css("opacity", ".75");
+//         });
 
-    p.fadeIn('slow', function() {
-        p.css("opacity", "1");
-    });
-        result.push(2);
-        audio = new Audio('../music/simonSound3.mp3');
-        audio.play();
-        checkUserInput(a, result);
-    });
-    $(".bottom-right-circle").click(function() {
-    	p = $(".bottom-right-circle");
-    	    p.fadeOut('slow', function() {
-        p.css("opacity", ".75");
-    });
+//         p.fadeIn('slow', function() {
+//             p.css("opacity", "1");
+//         });
+//         result.push(2);
+//         audio = new Audio('../music/simonSound3.mp3');
+//         audio.play();
+//          isInputCorrect = checkUserInput(runArray, result);
+//     });
+//     $(".bottom-right-circle").click(function() {
+//         p = $(".bottom-right-circle");
+//         p.fadeOut('slow', function() {
+//             p.css("opacity", ".75");
+//         });
 
-    p.fadeIn('slow', function() {
-        p.css("opacity", "1");
-    });
-        result.push(3);
-        audio = new Audio('../music/simonSound4.mp3');
-        audio.play();
-        checkUserInput(a, result);
-    });
+//         p.fadeIn('slow', function() {
+//             p.css("opacity", "1");
+//         });
+//         result.push(3);
+//         audio = new Audio('../music/simonSound4.mp3');
+//         audio.play();
+//         isInputCorrect = checkUserInput(runArray, result);
+//     });
+// }
 
-
-}
-
-function checkUserInput(a, result) {
-    for (i = 0; i < result.length; i++) {
-        if (a[i] != result[i]) {
-            //alert("no go");
-            if (strictMode){
-            	// restart game
-            } else {
-            	// restart round
-            }
-            // depending on mode, either start the round over, or reset.
+function check(runArray, result) {
+   for (i =0; i < result.length; i++){
+    if (result[i] != runArray[i]){
+        console.log("no go");
+    }
+    else {
+        if (result.length != runArray.length){
+            console.log("do nothing");
+        }
+        else{
+            console.log("won this round");
+            alert("ready for next round ?");
+            nextRound();
         }
     }
+   }
 }
+function nextRound(){
+    console.log(" ");
+    console.log("New Round");
+    count++;
+    console.log(a);
+     console.log(count);
+    displayArray(a, count);
+}
+
+
+
