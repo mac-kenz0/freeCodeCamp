e911178b21f08b40fe05a0b921fd50400240839b
mac-kenz0 <jordanm8118@gmail.com>
2017-04-10 17:59:55 -0400

    refined truncate-string js

--- basic-algorithm-scripting/truncate-a-string.js
+++ basic-algorithm-scripting/truncate-a-string.js
@@ -1,30 +1,20 @@
-
 function truncateString(str, num) {
-  // Clear out that junk in your trunk
-  // num says, you only want 14 elements in the return array
-  // the eclipse takes up three dots
-  var yolo;
 
-  if (str.length == num || str.length < num){
+  if (str.length == num || str.length < num) {
+    return str;
+  }
+
+
+  if (num > 3) {
+    str = str.slice(0, num - 3);
+  } else if (num < 3) {
+    str = str.slice(0, num);
+
+  }
+
+  str += "...";
+  console.log(str);
   return str;
-  }
-  if (str.length > num ){
-  	if (num - 3 > 0) {
-    	yolo = str.slice(0, num - 3);
-    }
-   else if ( num - 3 < 0){
-   		yolo = str.slice(0, num);
-   }
-   else{
-   	yolo = str;
-   }
-  }
- 
-  yolo = yolo + "...";
- 
-//  var test = "Peter Piper...";
-  //cconsole.log(test.length);
-  return yolo;
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 11);
